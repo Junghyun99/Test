369bd8ce278fb1b138174072e38eddda606ec934
Junghyun99 <swqasw12345@gmail.com>
2024-10-06 11:26:26 +0900

    js 업데이트

---
+++ site/assets/js/login.js
@@ -0,0 +1,11 @@
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // 로그인 검증
+    if (username === 'user' && password === 'pass') {
+        window.location.href = '../index.html';  // 로그인 성공 시 홈 페이지로 이동
+    } else {
+        alert('유효한 정보가 아닙니다.');  // 팝업 경고창
+    }
+}
