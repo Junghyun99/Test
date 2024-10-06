215aedab748b5e7cc2303546aabcaad46ac97b7c
Junghyun99 <swqasw12345@gmail.com>
2024-10-06 13:10:27 +0900

    login.js 업데이트

--- site/assets/js/login.js
+++ site/assets/js/login.js
@@ -4,7 +4,7 @@ function login() {
 
     // 로그인 검증
     if (username === 'user' && password === 'pass') {
-        window.location.href = '../../pages/home.html';  // 로그인 성공 시 홈 페이지로 이동
+        window.location.href = '../pages/home.html';  // 로그인 성공 시 홈 페이지로 이동
     } else {
         alert('유효한 정보가 아닙니다.');  // 팝업 경고창
     }
