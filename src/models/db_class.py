9863ffb143e9a38d7d39ca8096a0d6483c0873aa
Junghyun99 <swqasw12345@gmail.com>
2024-10-27 11:16:01 +0900

    db_class.py 업데이트

--- src/models/db_class.py
+++ src/models/db_class.py
@@ -6,19 +6,19 @@ class BaseDB(ABC):
         pass
 
     @abstractmethod
-    def add_data(self, query):
+    def add_data(self, query, data):
         pass
 
     @abstractmethod
-    def get_data(self, query):
+    def get_data(self, query, data):
         pass
 
     @abstractmethod
-    def delete_data(self, query):
+    def delete_data(self, query, data):
         pass
 
     @abstractmethod
-    def update_data(self, query):
+    def update_data(self, query, data):
         pass
 
     @abstractmethod
