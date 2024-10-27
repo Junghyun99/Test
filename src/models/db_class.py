3176ce34fb82ae185783706ebdb547c6b2847554
Junghyun99 <swqasw12345@gmail.com>
2024-10-27 11:05:01 +0900

    db_class.py 업데이트

--- src/models/db_class.py
+++ src/models/db_class.py
@@ -1,1 +1,26 @@
-#test
+from abc import ABC, abstractmethod
+
+class BaseDB(ABC):
+    @abstractmethod
+    def _create_table(self):
+        pass
+
+    @abstractmethod
+    def add_data(self, transaction):
+        pass
+
+    @abstractmethod
+    def get_data(self, stock_name):
+        pass
+
+    @abstractmethod
+    def delete_data(self, transaction_id):
+        pass
+
+    @abstractmethod
+    def update_data(self, transaction_id, quantity, price):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
