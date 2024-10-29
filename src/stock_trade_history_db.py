0f196c4ff0b9d0918c933f683b816bbc35ab3a02
Junghyun99 <swqasw12345@gmail.com>
2024-10-29 23:53:20 +0900

    stock_trade_history_db.py 업데이트

--- src/stock_trade_history_db.py
+++ src/stock_trade_history_db.py
@@ -7,16 +7,20 @@ class StockTradeHistoryDB:
 
     def _createTable(self):
         with self.conn:
-            self.conn.execute('''CREATE TABLE IF NOT EXISTS history 
-                              (
-                              id INTEGER PRIMARY KEY AUTOINCREMENT,
-                              date TEXT DEFAULT (datetime('now')),
-                              type TEXT NOT NULL CHECK (type IN ('BUY', 'SELL')),
-                              symbol TEXT NOT NULL,
-                              amount INTEGER NOT NULL CHECK (amount > 0),
-                              price REAL NOT NULL,
-                              fee REAL NOT NULL
-                              )''')
+            self.conn.execute('''CREATE TABLE trade_history (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,         -- 고유 식별자
+    stock_name TEXT NOT NULL,                     -- 종목 이름
+   나라 TEXT NOT NULL, 
+    transaction_id TEXT NOT NULL,                 -- 거래번호
+    country_code TEXT NOT NULL,
+    trade_round INTEGER,
+    trade_type TEXT CHECK(trade_type IN ('buy', 'sell')),  -- 거래 유형 (매수, 매도)
+    price REAL NOT NULL,                          -- 거래 단가
+    amount REAL NOT NULL,                         -- 거래 수량
+    total_value REAL GENERATED ALWAYS AS (price * amount), -- 총 거래 금액
+    status TEXT CHECK(status IN ('completed', 'processing' )), -- 거래 상태 (완료, 진행중)
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP  -- 거래 시간
+)''')
             
     def loadHistoryDate(self,date1, date2):
         cursor = self.conn.cursor()
