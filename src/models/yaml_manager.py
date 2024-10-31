a8791f6123e436b15d4226c64015974d2ab449b7
Junghyun99 <swqasw12345@gmail.com>
2024-10-31 15:34:12 +0000

    yaml 수정

--- src/models/yaml_manager.py
+++ src/models/yaml_manager.py
@@ -23,28 +23,28 @@ class YamlManager(ABC):
     def create(self, new_entry):
         """KR_STOCK 항목에 새 데이터를 추가."""
         data = self._read()
-        if COUNTRY_CODE not in data:
-            data[COUNTRY_CODE] = []
-      data[Country_code].append(new_entry)
+        if self.COUNTRY_CODE not in data:
+            data[self.COUNTRY_CODE] = []
+        data[self.COUNTRY_CODE].append(new_entry)
         self._write(data)
 
     @abstractmethod
     def read(self, identifier):
         """KR_STOCK 데이터 가져오기."""
         data = self.read() 
-        if COUNTRY_CODE not in data:
+        if self.COUNTRY_CODE not in data:
             return None
 
         if identifier:
-            return [entry for entry in data[COUNTRY_CODE] if entry.get("code") == identifier] 
-        return data[COUNTRY_CODE] 
+            return [entry for entry in data[self.COUNTRY_CODE] if entry.get("code") == identifier] 
+        return data[self.COUNTRY_CODE] 
 
     @abstractmethod
     def update(self, identifier, updated_data):
         """KR_STOCK에서 특정 항목 수정."""
         data = self._read()
-        if COUNTRY_CODE in data:
-            for entry in data[COUNTRY_CODE]:
+        if self.COUNTRY_CODE in data:
+            for entry in data[self.COUNTRY_CODE]:
                 if entry.get("code") == identifier:
                     entry.update(updated_data)
                     self._write(data)
@@ -55,10 +55,10 @@ class YamlManager(ABC):
     def delete(self, identifier):
         """KR_STOCK에서 특정 항목 삭제."""
         data = self._read()
-        if COUNTRY_CODE in data:
-            original_length = len(data[COUNTRY_CODE])
-            data[COUNTRY_CODE] = [entry for entry in data[COUNTRY_CODE] if entry.get("code") != identifier]
-            if len(data[COUNTRY_CODE]) < original_length:
+        if self.COUNTRY_CODE in data:
+            original_length = len(data[self.COUNTRY_CODE])
+            data[self.COUNTRY_CODE] = [entry for entry in data[self.COUNTRY_CODE] if entry.get("code") != identifier]
+            if len(data[self.COUNTRY_CODE]) < original_length:
                 self._write(data)
                 return True
         return False
--- src/service/yaml/yaml_kr_manager.py
+++ src/service/yaml/yaml_kr_manager.py
@@ -1,4 +1,4 @@
-from import
+from src.models.yaml_manager import YamlManager
 
 class YamlKrManager(YamlManager):
     COUNTRY_CODE = "KR_STOCK" 
