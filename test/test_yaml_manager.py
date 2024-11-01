5fcd47e59ddcc0c286c7d037050fb591f40fb71d
Junghyun99 <swqasw12345@gmail.com>
2024-10-31 20:13:29 -0500

    test_yaml_manager.py 업데이트

--- test/test_yaml_manager.py
+++ test/test_yaml_manager.py
@@ -12,14 +12,59 @@ def temp_file(tmp_path):
 
 @pytest.fixture
 def sample_data() -> Dict:
-    return {
-        "name": "Samsung Electronics",
-        "code": "005930",
-        "orders": [
-            {"order": 1, "buy_price": 60000, "buy_rate": 5, "sell_rate": 3},
-            {"order": 2, "buy_price": 61000, "buy_rate": 5, "sell_rate": 3}
-        ]
-    }
+    return {US_STOCK:
+  - name: Apple
+    code: AAPL
+    orders:
+      - order: 1
+        buy_price: 150
+        buy_rate: 5
+        sell_rate: 3
+      - order: 2
+        buy_price: 155
+        buy_rate: 5
+        sell_rate: 3
+
+  - name: Amazon
+    code: AMZN
+    orders:
+      - order: 1
+        buy_price: 3200
+        buy_rate: 4
+        sell_rate: 2
+      - order: 2
+        buy_price: 3300
+        buy_rate: 4
+        sell_rate: 2
+      - order: 3
+        buy_price: 3400
+        buy_rate: 4
+        sell_rate: 2
+
+KR_STOCK:
+  - name: 삼성전자
+    code: 005930
+    orders:
+      - order: 1
+        buy_price: 70000
+        buy_rate: 6
+        sell_rate: 3
+      - order: 2
+        buy_price: 71000
+        buy_rate: 6
+        sell_rate: 3
+      - order: 3
+        buy_price: 72000
+        buy_rate: 6
+        sell_rate: 3
+
+  - name: 현대차
+    code: 005380
+    orders:
+      - order: 1
+        buy_price: 180000
+        buy_rate: 5
+        sell_rate: 4}
 
 @pytest.fixture
 def kr_stock_crud(temp_file):
