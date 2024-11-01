b9d3a5ce6c569c67bcd89d4b3ffb7cf0782139b9
Junghyun99 <swqasw12345@gmail.com>
2024-11-01 18:29:45 -0500

    yaml_us_manager.py 업데이트

--- src/service/yaml/yaml_us_manager.py
+++ src/service/yaml/yaml_us_manager.py
@@ -2,3 +2,5 @@ from src.models.yaml_manager import YamlManager
 
 class YamlUsManager(YamlManager):
     COUNTRY_CODE = "US_STOCK"
+    def __init__(self,file_path):
+        super().__init__(file_path)
