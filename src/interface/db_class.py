1d5bf9a04beb5741a3eb36c177a63f3179310aef
Junghyun99 <swqasw12345@gmail.com>
2024-11-01 20:09:56 -0500

    db_class.py 업데이트

--- src/interface/db_class.py
+++ src/interface/db_class.py
@@ -1,29 +1,23 @@
-from abc import ABC, abstractmethod
-
 class BaseDB(ABC):
-    @abstractmethod
+    
     def _create_table(self):
         pass
 
-    @abstractmethod
-    def add_data(self, query, data):
+    def create_data(self, query, data):
         cursor = self.conn.cursor()
         cursor.execute(query, data)
         self.conn.commit()  # 명시적 commit
-
-    @abstractmethod 
-    def get_data(self, query, data):
+ 
+    def read_data(self, query, data):
         cursor = self.conn.cursor()
         cursor.execute(query, data)
         return cursor.fetchall()
 
-    @abstractmethod
     def delete_data(self, query, data):
         cursor = self.conn.cursor()
         cursor.execute(query, data)
         self.conn.commit()  # 명시적 commit
 
-    @abstractmethod
     def update_data(self, query, data):
         cursor = self.conn.cursor()
         cursor.execute(query, data)
