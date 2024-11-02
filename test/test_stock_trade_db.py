23eba8b4a6c3e8f03b39649b4e12a166fdfac9e6
Junghyun99 <swqasw12345@gmail.com>
2024-11-01 20:31:07 -0500

    test_stock_trade_db.py 업데이트

--- test/test_stock_trade_db.py
+++ test/test_stock_trade_db.py
@@ -5,7 +5,7 @@ from src.service.repository.stock_trade_db import StockTradeDB
 
 @pytest.fixture
 def temp_file(tmp_path):
-    file_path = tmp_path
+    file_path = tmp_path / "test_stock_db.db"
     yield file_path
     if file_path.exists():
         os.remove(file_path)
@@ -13,4 +13,6 @@ def temp_file(tmp_path):
 
 @pytest.fixture
 def stock_db(temp_file):
-    return StockTradeDB(str(temp_file))
+    db = StockTradeDB(str(temp_file))
+    yield db
+    db.close()
