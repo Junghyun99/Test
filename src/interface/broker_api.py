62174ec6300ea3976b5b10647b954fb4e4b0aa7c
Junghyun99 <swqasw12345@gmail.com>
2024-11-06 07:39:55 -0600

    broker_api.py 업데이트

--- src/interface/broker_api.py
+++ src/interface/broker_api.py
@@ -26,3 +26,13 @@ class BrokerAPI(ABC):
     def get_order_book(self, symbol):
         """호가 검색 (매수호가와 매도호가 리스트 반환)"""
         pass
+
+    @abstractmethod
+    def amend_order(self, order_id, new_quantity, new_price):
+        """정정 기능: 대기 중인 거래의 수량이나 가격을 수정."""
+        pass
+
+    @abstractmethod
+    def cancel_order(self, order_id):
+        """취소 기능: 대기 중인 거래를 취소."""
+        pass
