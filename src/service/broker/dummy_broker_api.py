de7e80b391da83b5844c2fd44e6083196f30b69f
Junghyun99 <swqasw12345@gmail.com>
2024-11-06 20:31:55 -0600

    dummy_broker_api.py 업데이트

--- src/service/broker/dummy_broker_api.py
+++ src/service/broker/dummy_broker_api.py
@@ -40,7 +40,7 @@ class DummyBrokerAPI(BrokerAPI):
         self.order[order_id] = re_status
         return re_status
 
-    def get_order_book(self, symbol: str) -> Tuple[List[Tuple[float, int]], List[Tuple[float, int]]]:
+    def get_order_book(self, symbol):
         buy_orders = [(round(random.uniform(100, 500), 2), random.randint(1, 100)) for _ in range(5)]
         sell_orders = [(round(random.uniform(500, 1000), 2), random.randint(1, 100)) for _ in range(5)]
         return sorted(buy_orders, reverse=True), sorted(sell_orders)
