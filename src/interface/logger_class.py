b0a08cc272e207f21b559a3ee92b4205f90acb5f
Junghyun99 <swqasw12345@gmail.com>
2024-11-08 09:49:12 -0600

    logger_class.py 업데이트

--- src/interface/logger_class.py
+++ src/interface/logger_class.py
@@ -2,13 +2,13 @@ import logging
 from abc import ABC, abstractmethod
 
 class BaseLogger(ABC):
-    def __init__(self, logger_name: str, log_file: str, level=logging.INFO):
+    def __init__(self, logger_name, log_file, level=logging.INFO):
         self.logger = logging.getLogger(logger_name)
         self.logger.propagate = False
         self.logger.setLevel(level)
         self._setup_handlers(log_file)
 
-    def _setup_handlers(self, log_file: str):
+    def _setup_handlers(self, log_file):
         # 콘솔 핸들러
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(self._get_formatter())
