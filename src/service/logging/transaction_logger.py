8262ba4a3516003ecc34c00e157c22efba70e849
Junghyun99 <swqasw12345@gmail.com>
2024-11-08 03:39:35 -0600

    transaction_logger.py 업데이트

--- src/service/logging/transaction_logger.py
+++ src/service/logging/transaction_logger.py
@@ -3,3 +3,9 @@ from src.interface.logger_class import BaseLogger
 class TransactionLogger(BaseLogger):
     def __init__(self):
         super().__init__('TransactionLogger')
+
+
+
+    def log_transaction(self, stock_name: str, action: str, amount: int, price: float):
+        message = f"{stock_name} - {action} {amount} shares at {price}"
+        self.logger.info(message)
