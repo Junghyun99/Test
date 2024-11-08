2b583b0a61b73410def099cbcdbeb00c46e4ed15
Junghyun99 <swqasw12345@gmail.com>
2024-11-08 03:39:21 -0600

    logger_class.py 업데이트

--- src/service/logging/logger_class.py
+++ src/service/logging/logger_class.py
@@ -38,18 +38,6 @@ class BaseLogger(ABC):
         self.logger.error(message)
 
 
-
-
-
-
-
-
-
-
-    def log_transaction(self, stock_name: str, action: str, amount: int, price: float):
-        message = f"{stock_name} - {action} {amount} shares at {price}"
-        self.logger.info(message)
-
     
 
    
