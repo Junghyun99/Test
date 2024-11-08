9901aa4381faaba50db6feec398e43ff517726bb
Junghyun99 <swqasw12345@gmail.com>
2024-11-08 03:40:11 -0600

    interface 업데이트

---
+++ src/interface/logger_class.py
@@ -0,0 +1,38 @@
+import logging
+from abc import ABC, abstractmethod
+
+class BaseLogger(ABC):
+    def __init__(self, logger_name: str, log_file: str, level=logging.INFO):
+        self.logger = logging.getLogger(logger_name)
+        self.logger.propagate = False
+        self.logger.setLevel(level)
+        self._setup_handlers(log_file)
+
+    def _setup_handlers(self, log_file: str):
+        # 콘솔 핸들러
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(self._get_formatter())
+        self.logger.addHandler(console_handler)
+
+        # 파일 핸들러
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(self._get_formatter())
+        self.logger.addHandler(file_handler)
+
+    def _get_formatter(self):
+        return logging.Formatter('%(asctime)s - %(process)d - %(threadName)s - %(module)s - %(lineno)d - %(name)s - %(levelname)s - %(message)s')
+
+    def get_logger(self):
+        return self.logger
+
+    def log_debug(self, message):
+        self.logger.debug(message)
+
+    def log_info(self, message):
+        self.logger.info(message)
+
+    def log_warning(self, message):
+        self.logger.warning(message)
+
+    def log_error(self, message):
+        self.logger.error(message)
