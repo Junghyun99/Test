d413e8346aa9c22aa4c1337d854271762e5f57d3
Junghyun99 <swqasw12345@gmail.com>
2024-11-18 09:28:14 -0600

    broker_manager.py 업데이트

--- src/service/broker/broker_manager.py
+++ src/service/broker/broker_manager.py
@@ -2,6 +2,19 @@ class BrokerManager:
     def __init__(self, broker):
         self.broker = broker
 
-    def 
-
       status= ["cancel", "pending", "complete"]
+
+    def get_current_price(self, symbol):
+        return self.broker.get_current_price(symbol)
+
+    def place_market_order(self, symbol, quantity, order_type):      
+        order_id = self.broker.place_market_order(symbol, quantity, order_type)
+        if 
+
+
+        return get_current_price
+
+    def place_limit_order(self, symbol, quantity, price, order_type):
+        order_id = self.broker.place_limit_order(symbol, quantity, price, order_type)
+
+        return order_id
