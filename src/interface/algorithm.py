30dd2cbe3be686589bac8b315e373181e05d53ec
Junghyun99 <swqasw12345@gmail.com>
2024-11-18 08:40:04 -0600

    algorithm.py 업데이트

--- src/interface/algorithm.py
+++ src/interface/algorithm.py
@@ -1,1 +1,13 @@
-class
+from abc import ABC, abstractmethod
+
+
+class Algorithm(ABC):   
+    @abstractmethod
+    def run_algorithm(self, symbol):
+        """주가 검색"""
+        pass
+
+    @abstractmethod
+    def place_market_order(self, symbol, quantity, order_type):
+        """시장가 매수/매도 (order_type은 'buy' 또는 'sell')"""
+        pass 
