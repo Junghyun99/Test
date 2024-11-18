b3e31e543d4e9478301a1770296bc55fb1ea1fdf
Junghyun99 <swqasw12345@gmail.com>
2024-11-18 09:50:02 -0600

    magicsplit_algorithm.py 업데이트

--- src/service/algorithm/magicsplit_algorithm.py
+++ src/service/algorithm/magicsplit_algorithm.py
@@ -3,8 +3,8 @@ from src.util.price_calculator import PriceCalulator
 
 
 class MagicSplit(Algorithm):
-    def __init__(self, broker, trade_db_manager, yaml_manager):        
-        self.broker = broker
+    def __init__(self, broker_manager, trade_db_manager, yaml_manager):        
+        self.broker_manager = broker_manager
         self.trade_db_manager = trade_db_manager
         self.yaml_manager = yaml_manager
 
@@ -17,12 +17,19 @@ class MagicSplit(Algorithm):
         target_sell_price = PriceCalulator.calculate_price(buy_price, sell_rate, False)
 
         # 현재 가격을 API로 받아오기
-        current_price = self.broker.get_current_price(code)
+        current_price = self.broker_manager.get_current_price(code)
 
         # 매수 또는 매도 조건 확인
         if current_price <= target_buy_price:
-self.broker.place_market_order(code, quantity, "BUY")
+result = self.broker_manager.place_market_order(code, quantity, "BUY")
+        # 매수 성공시
+        self.trade_db_manager. 히스토리 추가       
+        yaml의 다음 차수정보 읽어와서
+        모니터링 디비 다음차수로 수정 
+
+
         elif current_price >= target_sell_price:
-            self.broker.place_market_order(code, quantity, "SELL")
+            result = self.broker_manager.place_market_order(code, quantity, "SELL")
+        # 매도 성공시
         else:
             pass
