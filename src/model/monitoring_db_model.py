e85a29cdb153425ebfc587fe36951840d4f33363
Junghyun99 <swqasw12345@gmail.com>
2024-11-20 13:26:37 +0000

    model 수정

--- src/model/monitoring_db_model.py
+++ src/model/monitoring_db_model.py
@@ -2,13 +2,13 @@ from dataclasses import dataclass
 
 @dataclass
 class MonitoringData:
-    stock_name
-    code
-    country_code
-    trade_round
-    price 
-    buy_rate
-    sell_rate
+    stock_name: str
+    code: str
+    country_code: str
+    trade_round: int
+    price : float
+    buy_rate: int
+    sell_rate: int
 
     def to_tuple(self, field_order):
         # 필드 이름 순서에 따라 튜플 생성
