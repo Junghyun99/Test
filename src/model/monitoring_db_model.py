f4d6f2ceb7d40850b7864654cec088bbf7a8c5c9
Junghyun99 <swqasw12345@gmail.com>
2024-11-19 18:23:48 -0600

    model 업데이트

---
+++ src/model/monitoring_db_model.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+@dataclass
+class MonitoringData:
+    stock_name
+    code
+    country_code
+    buy_price
+    trade_round
+    buy_rate
+    sell_rate
+
+    def to_tuple(self, field_order):
+        # 필드 이름 순서에 따라 튜플 생성
+        return tuple(getattr(self, field) for field in field_order)
