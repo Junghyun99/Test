a2cd6324a3ecb0cbff84532c88b340a9cffda9f0
Junghyun99 <swqasw12345@gmail.com>
2024-11-19 18:35:00 -0600

    monitoring_db_model.py 업데이트

--- src/model/monitoring_db_model.py
+++ src/model/monitoring_db_model.py
@@ -13,3 +13,7 @@ class MonitoringData:
     def to_tuple(self, field_order):
         # 필드 이름 순서에 따라 튜플 생성
         return tuple(getattr(self, field) for field in field_order)
+
+
+# 기본 필드 순서
+FIELD_ORDER = ["stock_name", "code", "country_code", "trade_round", "price", "buy_rate", "sell_rate"]
