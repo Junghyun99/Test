95ed3ffb666a4c0c0a1e5d959b34795c2f90105b
Junghyun99 <swqasw12345@gmail.com>
2024-12-02 18:08:34 +0900

    test_logger_class.py 업데이트

--- test/test_logger_class.py
+++ test/test_logger_class.py
@@ -24,41 +24,50 @@ def logger(temp_file):
   
 def test_get_logger_name(logger):
     assert logger.get_logger().name == "TestLogger"
+    logger.get_logger().handlers.clear()
 
 def test_get_logger_level_default(logger):
     assert logger.get_logger().level == logging.INFO
+    logger.get_logger().handlers.clear()
 
 def test_get_logger_propagate(logger):
     assert not logger.get_logger().propagate
+    logger.get_logger().handlers.clear()
 
 
 def test_get_logger_handlers(logger):
     assert len(logger.get_logger().handlers) == 2  
     # Console and File handlers
+    logger.get_logger().handlers.clear()
 
 def test_get_logger_file_handler_type(logger):
     assert any(isinstance(handler, logging.FileHandler) for handler in logger.get_logger().handlers)
+    logger.get_logger().handlers.clear()
 
 def test_log_debug_enabled(logger):
     logger.log_debug("Debug message")
     assert logger.get_logger().isEnabledFor(logging.DEBUG) == False
+    logger.get_logger().handlers.clear()
 
 def test_log_debug_message_content(logger, caplog):
     with caplog.at_level(logging.DEBUG):
         logger.log_debug("Debug message")
     assert "Debug message" not in caplog.text
+    logger.get_logger().handlers.clear()
 
 def test_log_info_level(logger, caplog):
     logger.get_logger().propagate = True
     with caplog.at_level(logging.INFO):
         logger.log_info("Check level")
     assert "INFO" in caplog.text
+    logger.get_logger().handlers.clear()
 
 def test_log_info_enabled_when_above_level(logger, caplog):
     logger.get_logger().propagate = True
     with caplog.at_level(logging.WARNING):
         logger.log_warning("This appear")
     assert "This appear" in caplog.text
+    logger.get_logger().handlers.clear()
 
 def test_log_info_file_output(logger,temp_file):
     print(logger.get_logger().handlers)
@@ -67,6 +76,7 @@ def test_log_info_file_output(logger,temp_file):
     assert temp_file.exists() 
     with open(temp_file, "r") as file:
         assert "File output test" in file.read()
+    logger.get_logger().handlers.clear()
 
 def test_log_warning_message(logger, caplog):
     logger.get_logger().propagate = True
@@ -74,6 +84,7 @@ def test_log_warning_message(logger, caplog):
         logger.log_warning("Warning message")
     assert "Warning message" in caplog.text
     assert "WARNING" in caplog.text
+    logger.get_logger().handlers.clear()
 
 def test_log_error_message(logger, caplog):
     logger.get_logger().propagate = True
@@ -81,6 +92,7 @@ def test_log_error_message(logger, caplog):
         logger.log_error("Error message")
     assert "Error message" in caplog.text
     assert "ERROR" in caplog.text
+    logger.get_logger().handlers.clear()
 
 
 def test_multiple_log_levels(logger, caplog):
@@ -94,23 +106,28 @@ def test_multiple_log_levels(logger, caplog):
     assert "Info message" in caplog.text
     assert "Warning message" in caplog.text
     assert "Error message" in caplog.text
+    logger.get_logger().handlers.clear()
 
 
 def test_logger_info(logger):
     logger.log_info("This is an info message")
     assert logger.get_logger().isEnabledFor(logging.INFO)
+    logger.get_logger().handlers.clear()
 
 def test_logger_warning(logger):
     logger.log_warning("This is a warning message")
     assert logger.get_logger().isEnabledFor(logging.WARNING)
+    logger.get_logger().handlers.clear()
 
 def test_logger_error(logger):
     logger.log_error("This is an error message")
     assert logger.get_logger().isEnabledFor(logging.ERROR)
+    logger.get_logger().handlers.clear()
 
 def test_log_file_created(logger, temp_file):
     logger.log_info("Testing file creation")
     assert os.path.exists(temp_file)
+    logger.get_logger().handlers.clear()
 
 # Composite scenarios for BaseLogger
 def test_logger_multiple_levels(logger):
@@ -122,4 +139,5 @@ def test_logger_multiple_levels(logger):
     assert logger.get_logger().isEnabledFor(logging.INFO)
     assert logger.get_logger().isEnabledFor(logging.WARNING)
     assert logger.get_logger().isEnabledFor(logging.ERROR)
+    logger.get_logger().handlers.clear()
 
