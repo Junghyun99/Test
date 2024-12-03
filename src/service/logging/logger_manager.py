f1fee5509ad2ff1c1b9dc8b2426ab458df06c509
Junghyun99 <swqasw12345@gmail.com>
2024-12-03 11:15:44 +0900

    logger_manager.py 업데이트

--- src/service/logging/logger_manager.py
+++ src/service/logging/logger_manager.py
@@ -2,16 +2,34 @@ from src.service.logging.system_logger import SystemLogger
 from src.service.logging.transaction_logger import TransactionLogger
 
 
+import yaml
+from pathlib import Path
+
+
+
 class LoggerManager:
     def __init__(self):
         self.loggers = {}
+        self._set_yaml_config()
+
+    def _set_yaml_config(self):
+        # YAML 설정 로드
+        with open("src/config/logging.yaml", "r") as file:
+            config = yaml.safe_load(file)
+
+        log_dir = Path(config['log_dir'])
+        self.system_log_file = log_dir / config['system_log_file']
+        self.transaction_log_file = log_dir / config['transaction_log_file']
+
+        # 디렉토리 생성 (없으면 생성)
+        log_dir.mkdir(parents=True, exist_ok=True)
 
     def get_logger(self, logger_type):
         if logger_type not in self.loggers:
             if logger_type.upper() == 'SYSTEM':
-                self.loggers[logger_type] = SystemLogger("SystemLogger", "system_log_file.log")
+                self.loggers[logger_type] = SystemLogger("SystemLogger", self.system_log_file)
             elif logger_type.upper() == 'TRANSACTION':
-                self.loggers[logger_type] = TransactionLogger("TransactionLogger","transaction_log_file.log")
+                self.loggers[logger_type] = TransactionLogger("TransactionLogger",self.transaction_log_file)
             else:
                 raise ValueError(f"Unknown logger type: {logger_type}")
         return self.loggers[logger_type]
