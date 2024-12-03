00b3a4fa01121695dd0b0eef50d423f38b3b2521
Junghyun99 <swqasw12345@gmail.com>
2024-12-03 16:45:59 +0000

    monitoring logging 추가

--- src/service/repository/monitoring_manager.py
+++ src/service/repository/monitoring_manager.py
@@ -3,6 +3,10 @@ import os
 from src.service.repository.monitoring_db import MonitoringDB
 from src.model.monitoring_db_model import MonitoringData
 from src.util.enums import CountryCode, QueryOp
+from src.service.logging.logger_manager import logger_manager
+
+system_logger = logger_manager.get_logger('SYSTEM')
+
 
 class MonitoringManager:
     COUNTRY_CODE = CountryCode.KR
@@ -10,11 +14,13 @@ class MonitoringManager:
     def __init__(self, algorithm):
         self.db = MonitoringDB()
         self.algorithm = algorithm
+        system_logger.log_info("create MonitoringManager instance, init")
 
     def read_all_stocks(self, country_code):
         """모니터링 DB에서 모든 종목 읽기"""
         query = "SELECT * FROM monitoring WHERE country_code=?"
         data = (country_code,)
+        system_logger.log_info("read_all_stocks country code %s", country_code)
         return self.db.read_data(query, data)
 
     def add_stock_in_monitoring(self, stock_name, code, country_code, trade_round, price, quantity, buy_rate, sell_rate):
@@ -22,18 +28,22 @@ class MonitoringManager:
         query = '''INSERT INTO monitoring (stock_name, code, country_code, trade_round, price, quantity, buy_rate, sell_rate)
                    VALUES (?, ?, ?, ?, ?, ?, ?, ?)'''
         data = (stock_name, code, country_code, trade_round, price, quantity, buy_rate, sell_rate)
+        system_logger.log_info("add_stock_in_monitoring query %s, data %s", query, data)
         self.db.insert_data(query, data)
 
     def delete_stock_in_monitoring(self, code):
         """종목 삭제"""
         query = '''DELETE FROM monitoring WHERE code = ?'''
         data = (code,)
+        system_logger.log_info("delete_stock_in_monitoring query %s, data %s", query, data)
         self.db.delete_data(query, data)
  
     def update_stock_in_monitoring(self, stock_name, code, country_code, trade_round, price, quantity, buy_rate, sell_rate):
         query = '''UPDATE INTO monitoring SET trade_round =?, price=?, quantity=?, buy_rate=?, sell_rate=? WHERE code = ?'''
         data = (trade_round, price, quantity, buy_rate, sell_rate, code)
+        system_logger.log_info("update_stock_in_monitoring query %s, data %s", query, data)
         self.db.update_data(query, data)
+        
 
     def start_monitoring(self):
         """모든 종목을 멀티 쓰레드로 모니터링"""
@@ -41,6 +51,7 @@ class MonitoringManager:
         results = []
         errors = []
         stocks = self.read_all_stocks(self.COUNTRY_CODE.value)
+        system_logger.log_info("start_monitoring max_core %s", max_core)
         with ThreadPoolExecutor(max_workers=max_core) as executor:
             futures = [executor.submit(self.algorithm.run_algorithm, MonitoringData(*stock)) for stock in stocks]
 
@@ -61,7 +72,7 @@ class MonitoringManager:
         if errors:
         # 에러 상세 정보 출력
             for error in errors:
-                print(f"Error at index {error['index']} with item {error['item']}: {error['error']}")
+                system_logger.log_error("start_monitoring error %s", error)
             raise Exception("One or more errors occurred.")
 
 
