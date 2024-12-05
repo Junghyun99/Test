547215ce2bd36e917b12ff1b02c0382f29962264
Junghyun99 <swqasw12345@gmail.com>
2024-12-05 12:42:10 +0900

    main.py 업데이트

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
-from src.service.repository.monitoring_manager import MonitoringKrManager
+from src.service.repository.monitoring_manager import MonitoringKrManager, MonitoringUsManager
 from src.service.repository.trade_db_manager import TradeDbManager
 from src.service.algorithm.magicsplit_algorithm import MagicSplit
 from src.sevice.broker.broker_manager import BrokerManager
-from src.sevice.yaml.yaml_manager import YamlKrManager
+from src.sevice.yaml.yaml_manager import YamlKrManager, YamlUsManager 
 
 
+def run_common():
+    trade_db_manager = TradeDbManager()
+    algorithm = MagicSplit()
+    broker_manager = BrokerManager()
+
+
+def run_kr():
+    moni_manager = MonitoringKrManager()
+    yaml_manager = YamlKrManager()
+    
+
+def run_us():
+    moni_manager = MonitoringUsManager() 
+    yaml_manager = YamlUsManager()
