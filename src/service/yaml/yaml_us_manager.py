26a3c690790f0943fea1f5491fa83bc3b2b69b80
Junghyun99 <swqasw12345@gmail.com>
2024-12-05 09:32:27 +0900

    Update yaml_us_manager.py

--- src/service/yaml/yaml_us_manager.py
+++ src/service/yaml/yaml_us_manager.py
@@ -1,7 +1,1 @@
-from src.interface.yaml_manager import YamlManager 
-from src.util.enums import CountryCode
-
-class YamlUsManager(YamlManager):
-    COUNTRY_CODE = "US"
-    def __init__(self,file_path):
-        super().__init__(file_path)
+#
