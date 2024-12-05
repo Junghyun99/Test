44693f6d3e8b1f7030d2bac5180d5f16a5d595a2
Junghyun99 <swqasw12345@gmail.com>
2024-12-05 21:41:50 +0900

    main.py 업데이트

--- src/main.py
+++ src/main.py
@@ -7,18 +7,25 @@ from src.sevice.yaml.yaml_manager import YamlKrManager, YamlUsManager
 from src.service.broker.dummy_broker_api import DummyBrokerAPI
 
 file_path = "src/config/stock_round_config.yaml"
-def run_common():
-    trade_db_manager = TradeDbManager()
-    algorithm = MagicSplit()
-
-    broker_manager = BrokerManager(DummyBrokerAPI())
 
 
-def run_kr(algorithm):
-    moni_manager = MonitoringKrManager(algorithm)
-    yaml_manager = YamlKrManager(file_path)
-    
+def get_yaml_manager(country_code):
+    if country_code == CountryCode.KR:
+        return YamlKrManager(file_path)
+    else:
+        return YamlUsManager(file_path) 
 
-def run_us(algorithm):
-    moni_manager = MonitoringUsManager(algorithm) 
-    yaml_manager = YamlUsManager(file_path)
+def get_monitoring_manager(country_code, algo):
+    if country_code == CountryCode.KR:
+        return MonitoringKrManager(algo)
+    else:
+        return MonitoringUsManager(algo) 
+
+
+def run_common(country_code):
+    trade = TradeDbManager()    
+    yaml = get_yaml_manager(country_code)
+    broker = BrokerManager(DummyBrokerAPI())
+    algorithm = MagicSplit(broker, trade, yaml)
+    moni = get_monitoring_manager(country_code, algorithm)
+    moni.start_monitoring()
