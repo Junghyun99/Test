636daab03572636c3c4262bc4de51837ff19e1c0
Junghyun99 <swqasw12345@gmail.com>
2024-12-05 09:32:48 +0900

    main.py 업데이트

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from src.service.repository.monitoring_manager import MonitoringKrManager
 from src.service.repository.trade_db_manager import TradeDbManager
 from src.service.algorithm.magicsplit_algorithm import MagicSplit
 from src.sevice.broker.broker_manager import BrokerManager
-from src.sevice.yaml.yaml_manager import YamlManager
+from src.sevice.yaml.yaml_manager import YamlKrManager
 
 
