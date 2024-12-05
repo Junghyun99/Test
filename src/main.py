83c8cd17b664e7e6a6ae458ef4506608a44e0489
Junghyun99 <swqasw12345@gmail.com>
2024-12-05 09:23:39 +0900

    main.py 업데이트

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
-from src.service.repository.monitoring_manager import MonitoringManager
-from src.service.repository.trade_db_manager import TradeManager
+from src.service.repository.monitoring_manager import MonitoringKrManager
+from src.service.repository.trade_db_manager import TradeDbManager
+
+
 
