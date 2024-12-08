28d135af9fa83871b034cd28c202219616ddedf6
Junghyun99 <swqasw12345@gmail.com>
2024-12-08 22:34:15 +0900

    test_magicsplit_algorithm.py 업데이트

--- test/test_magicsplit_algorithm.py
+++ test/test_magicsplit_algorithm.py
@@ -101,7 +101,7 @@ def test_try_sell_stock_fail(setup_magic_split, mocker):
 
     moni_data = MonitoringData("StockA", "ABC123", "KR", 2, 1000, 10, 0.5, 1.5)
     yaml_mock_data = [{"orders": [{"order": 1, "buy_price": 950, "buy_rate": 0.5, "sell_rate": 1.5}, {"order": 2, "buy_price": 960, "buy_rate": 0.5, "sell_rate": 1.5}]}]
-    mock_yaml_manager.read_all.return_value = yaml_mock_data
+    mock_yaml_manager.read_by_id.return_value = yaml_mock_data
     mock_broker_manager.place_market_order.return_value = (True, None)
     mock_trade_db_manager.get_trade_round.return_value = (950, 10)
     # 2. 매도 실패
@@ -114,7 +114,7 @@ def test_try_sell_stock_first(setup_magic_split, mocker):
 
     moni_data = MonitoringData("StockA", "ABC123", "KR", 1, 1000, 10, 0.5, 1.5)
     yaml_mock_data = [{"orders": [{"order": 1, "buy_price": 950, "buy_rate": 0.5, "sell_rate": 1.5}, {"order": 2, "buy_price": 960, "buy_rate": 0.5, "sell_rate": 1.5}]}]
-    mock_yaml_manager.read_all.return_value = yaml_mock_data
+    mock_yaml_manager.read_by_id.return_value = yaml_mock_data
     mock_broker_manager.place_market_order.return_value = (True, None)
     mock_trade_db_manager.get_trade_round.return_value = (950, 10)
     # 3. 첫 번째 차수에서 매도 성공
