71832d486ca715280db17c269daea0539c9a6570
Junghyun99 <swqasw12345@gmail.com>
2024-12-12 04:57:28 +0900

    yaml_manager.py 업데이트

--- src/service/yaml/yaml_manager.py
+++ src/service/yaml/yaml_manager.py
@@ -3,10 +3,10 @@ from src.util.enums import CountryCode
 
 class YamlKrManager(YamlManager):
     COUNTRY_CODE = "KR"
-    def __init__(self,file_path):
+    def __init__(self,file_path = "src/config/stock_round_config.yaml"):
         super().__init__(file_path)
 
 class YamlUsManager(YamlManager):
     COUNTRY_CODE = "US"
-    def __init__(self,file_path):
+    def __init__(self,file_path = "src/config/stock_round_config.yaml"):
         super().__init__(file_path)
