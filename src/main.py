c4963926af909a9174417890d144d7dea33bac2f
Junghyun99 <swqasw12345@gmail.com>
2024-12-11 23:45:20 +0900

    main.py 업데이트

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import sys
+import argparse
+
 from src.service.repository.monitoring_manager import MonitoringKRManager, MonitoringUSManager
 from src.service.repository.trade_db_manager import TradeDBManager
 from src.service.algorithm.magicsplit_algorithm import MagicSplit
@@ -10,7 +12,40 @@ from src.util.enums import CountryCode
 
 file_path = "src/config/stock_round_config.yaml"
 
-def get_yaml_manager(country_code, file_path):
+
+class MainApp:
+    def __init__(self):
+        self.args = None
+        self.country_code = CountryCode.KR
+        self.parser_argument()
+        self.parse_country_code()
+
+    def parser_argument(self):
+        # ArgumentParser 객체 생성
+        parser = argparse.ArgumentParser(description="Run stock monitoring system")
+    
+        # country 인자를 추가 (선택적, 기본값은 'KR')
+        parser.add_argument(
+        "country",  # 명령행에서 받을 인자 이름
+        nargs="?",  # 선택적 인자
+        choices=["KR", "US"],  # 유효한 값 제한
+        default="KR",  # 기본값
+        help="Country code for the stock monitoring system (KR/US)"
+    )
+      
+        # 명령행 인자 파싱
+        self.args = parser.parse_args()
+
+    def parse_country_code():
+        country_code_map = {
+        "KR": CountryCode.KR,
+        "US": CountryCode.US,
+        }
+        self.country_code = country_code_map.get(self.args.country.uppet())
+
+
+
+    def get_yaml_manager(country_code, file_path):
     manager_classes = {
         CountryCode.KR: YamlKrManager,
         CountryCode.US: YamlUsManager,
@@ -38,34 +73,11 @@ def run(country_code):
         trade.close_db()
         moni.close_db()
 
-def parse_country_code(arg):
-    country_code_map = {
-        "KR": CountryCode.KR,
-        "US": CountryCode.US,
-    }
-    return country_code_map.get(arg.upper())
+    
 
 
-
-def main():
-    import argparse
-
-    # ArgumentParser 객체 생성
-    parser = argparse.ArgumentParser(description="Run stock monitoring system")
     
-    # country 인자를 추가 (선택적, 기본값은 'KR')
-    parser.add_argument(
-        "country",  # 명령행에서 받을 인자 이름
-        nargs="?",  # 선택적 인자
-        choices=["KR", "US"],  # 유효한 값 제한
-        default="KR",  # 기본값
-        help="Country code for the stock monitoring system (KR/US)"
-    )
-    
-    
-    # 명령행 인자 파싱
-    args = parser.parse_args()
-    
+        
     try:
         # country 인자를 CountryCode로 변환
         country_code = parse_country_code(args.country)
