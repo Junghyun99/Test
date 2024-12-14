40377342605e48c5430709bc8e421b90b8fbeb49
Junghyun99 <swqasw12345@gmail.com>
2024-12-14 10:18:30 +0900

    yaml_manager.py 업데이트

--- src/service/yaml/yaml_manager.py
+++ src/service/yaml/yaml_manager.py
@@ -1,6 +1,10 @@
 from src.interface.yaml_manager import YamlManager
 from src.util.enums import CountryCode
 
+from src.service.logging.logger_manager import logger_manager
+
+system_logger = logger_manager.get_logger('SYSTEM')
+
 class YamlKrManager(YamlManager):
     COUNTRY_CODE = "KR"
     def __init__(self,file_path = "src/config/stock_round_config.yaml"):
