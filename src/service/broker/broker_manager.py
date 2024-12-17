3c6ce8942b2aa2359ffbc5754ff2862d1d735309
Junghyun99 <swqasw12345@gmail.com>
2024-12-17 09:26:10 +0900

    broker_manager.py 업데이트

--- src/service/broker/broker_manager.py
+++ src/service/broker/broker_manager.py
@@ -1,30 +1,28 @@
 import time
-from src.service.logging.logger_manager import logger_manager
-
-transaction_logger = logger_manager.get_logger('TRANSACTION')
 
 class BrokerManager:
-    def __init__(self, broker):
+    def __init__(self, broker, logger):
         self.broker = broker
-        transaction_logger.log_info("create BrokerManager instance, init")
+        self.logger = logger
+        self.logger.log_info("create BrokerManager instance, init")
 
     def get_current_price(self, symbol):
         price = self.broker.get_current_price(symbol)
-        transaction_logger.log_info("get_current_price code %s, price %s", symbol, price)        
+        self.logger.log_info("get_current_price code %s, price %s", symbol, price)        
         return price
 
     def place_market_order(self, symbol, quantity, order_type):      
         order_id = self.broker.place_market_order(symbol, quantity, order_type)
 
-        transaction_logger.log_transaction(code, order_type, quantity, 0) #todo      
+        self.logger.log_transaction(code, order_type, quantity, 0) #todo      
 
-        transaction_logger.log_info("place_market_order code %s, quantity %s type %s id %s", symbol, quantity, order_type, order_id)
+        self.logger.log_info("place_market_order code %s, quantity %s type %s id %s", symbol, quantity, order_type, order_id)
 
         result = False
         count = 0
         while count < 10:
             status = self.broker.get_order_status(order_id)
-            transaction_logger.log_info("place_market_order status %s, count %s", status, count) 
+            self.logger.log_info("place_market_order status %s, count %s", status, count) 
             if status == "complete":
                 result = True
                 break
@@ -33,7 +31,7 @@ class BrokerManager:
             count += 1
 
         if count == 10:
-            transaction_logger.log_info("place_market_order cancel") 
+            self.logger.log_info("place_market_order cancel") 
             self.broker.cancel_order(order_id)
         return result
     # self.trade_db_manager. 히스토리 추가, 브로커에서?해도될듯?
@@ -42,16 +40,16 @@ class BrokerManager:
         order_id = self.broker.place_limit_order(symbol, quantity, price, order_type)
 
 
-        transaction_logger.log_transaction(code, order_type, quantity, price)
+        self.logger.log_transaction(code, order_type, quantity, price)
 
-        transaction_logger.log_info("place_limit_order code %s, quantity %s price %s type %s id %s", symbol, quantity, price, order_type, order_id)
+        self.logger.log_info("place_limit_order code %s, quantity %s price %s type %s id %s", symbol, quantity, price, order_type, order_id)
 
         result = False
         info = ()
         count = 0
         while count < 10:
             status = self.broker.get_order_status(order_id)
-            transaction_logger.log_info("place_limit_order status %s, count %s", status, count) 
+            self.logger.log_info("place_limit_order status %s, count %s", status, count) 
             if status == "complete":
                 info = self.broker.get_order_info(order_id)
                 result = True
@@ -61,6 +59,6 @@ class BrokerManager:
             count += 1
 
         if count == 10:
-            transaction_logger.log_info("place_limit_order cancel")     
+            self.logger.log_info("place_limit_order cancel")     
             self.broker.cancel_order(order_id)
         return (result, info)
