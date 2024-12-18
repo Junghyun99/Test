61057970187df32291f7282144163c34ad65d3a5
Junghyun99 <swqasw12345@gmail.com>
2024-12-19 08:49:29 +0900

    test_historical_run.py 업데이트

--- test/test_historical_run.py
+++ test/test_historical_run.py
@@ -3,6 +3,33 @@ import pytest
 from datetime import datetime, timedelta
 from freezegun import freeze_time
 
+
+def mock_get_
+
+
+@pytest.mark.large_test
+class TestHistoricalPrice:
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        # 각 테스트 메서드 실행 전에 실행되는 setup 코드
+        pass
+
+    def test_get_price(mocker):
+    # 특정 함수만 모킹
+    mocker.patch(".method_to_mock", return_value="Mocked Response")
+
+    obj = ExampleClass()
+
+    # 모킹된 함수 확인
+    assert obj.method_to_mock() == "Mocked Response"
+
+    # 다른 메서드는 원래 동작 유지
+    assert obj.method_to_keep() == "Keep Original Behavior"
+
+
+
+
 @pytest.mark.large_test
 class TestHistoricalRun:
 
