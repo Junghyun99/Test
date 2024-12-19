360063ead78e3c6a0ae9f5cdc03989bc5d0e4c0b
Junghyun99 <swqasw12345@gmail.com>
2024-12-19 11:18:26 +0900

    test_historical_run.py 업데이트

--- test/test_historical_run.py
+++ test/test_historical_run.py
@@ -26,11 +26,9 @@ class TestHistoricalPrice:
         # CSV 파일 읽기
         file_name = f"test/csv/historical_data_{symbol}.csv"
         df = pd.read_csv(file_name)
-        df_price = df[df["Date"] == "12/11/2024"]
-        print(df_price["Price"].values[0])
+        df_price = df[df["Date"] == "12/11/2024"]       
 
-
-        assert self.broker.get_current_price(symbol) == 2
+        assert df_price["Price"].values[0] == 224.23
 
 
 
