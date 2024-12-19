5a252f0902342e633f6aaa03ded3eead3d61c8c8
Junghyun99 <swqasw12345@gmail.com>
2024-12-19 20:19:17 +0900

    monitoring_db.py 업데이트

--- src/service/repository/monitoring_db.py
+++ src/service/repository/monitoring_db.py
@@ -15,7 +15,7 @@ class MonitoringDB(BaseDB):
                        stock_name TEXT NOT NULL,                     -- 종목 이름
                        code TEXT NOT NULL UNIQUE,                            -- 심볼 혹은 번호
                        country_code TEXT NOT NULL, 
-                       trade_round INTEGER CHECK(typeof(trade_round) == 'integer' AND trade_round > 0), --라운드
+                       trade_round INTEGER CHECK(typeof(trade_round) == 'integer'), --라운드
                        price REAL NOT NULL CHECK(typeof(price) == 'real' AND price > 0),     -- 거래 단가
                        quantity INTEGER NOT NULL CHECK(typeof(quantity) == 'integer' AND quantity > 0), -- 수량
                        buy_rate INTEGER NOT NULL CHECK(typeof(buy_rate) == 'integer' AND buy_rate > 0 AND buy_rate < 100),
