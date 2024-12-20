69dc6a0590021c4c7c2796229f847c3e2749c689
Junghyun99 <swqasw12345@gmail.com>
2024-12-20 13:34:52 +0900

    test_monitoring_manager.py 업데이트

--- test/test_monitoring_manager.py
+++ test/test_monitoring_manager.py
@@ -26,8 +26,8 @@ def test_read_all_stocks_empty_db(setup_manager):
 
 def test_read_all_stocks_with_data(setup_manager):
     mock_data = [
-        ("Samsung", "005930", "KR", 1, 70000, 20, 5, 10),
-        ("Hyundai", "005380", "KR", 2, 180000, 6, 4, 8)
+        (1, "Samsung", "005930", "KR", 1, 70000, 20, 5, 10),
+        (1, "Hyundai", "005380", "KR", 2, 180000, 6, 4, 8)
     ]
     manager, _, mock_db = setup_manager
     mock_db.read_data.return_value = mock_data
@@ -55,37 +55,37 @@ def test_read_all_stocks_exception(setup_manager):
 def test_add_stock_successful(setup_manager):
     manager, _, mock_db = setup_manager
     mock_db.insert_data.return_value = None
-    manager.add_stock_in_monitoring("StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5)
+    manager.add_stock_in_monitoring(1, "StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5)
     mock_db.insert_data.assert_called_once()
     
 def test_add_stock_missing_data(setup_manager):
     manager, _, mock_db = setup_manager
     with pytest.raises(TypeError):
-        manager.add_stock_in_monitoring("StockA", "123", "KR", 1, 1000)
+        manager.add_stock_in_monitoring(1, "StockA", "123", "KR", 1, 1000)
 
 def test_add_stock_invalid_query(setup_manager):
     manager, _, mock_db = setup_manager
     mock_db.insert_data.side_effect = Exception("sqlite3.IntegrityError")
     with pytest.raises(Exception):
-        manager.add_stock_in_monitoring("StockA", 123, "KR", "invalid", 1000, 10, 0.5, "invalid")
+        manager.add_stock_in_monitoring(1, "StockA", 123, "KR", "invalid", 1000, 10, 0.5, "invalid")
 
 def test_add_stock_Exception(setup_manager):
     manager, _, mock_db = setup_manager
     mock_db.insert_data.side_effect = Exception("DB Error")
     with pytest.raises(Exception):
-        manager.add_stock_in_monitoring("StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5)
+        manager.add_stock_in_monitoring(1, "StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5)
 
 def test_add_stock_duplicate_entry(setup_manager):
     manager, _, mock_db = setup_manager
-    manager.add_stock_in_monitoring("StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5)
+    manager.add_stock_in_monitoring(1, "StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5)
     mock_db.insert_data.side_effect = Exception("Unique constraint failed")
     with pytest.raises(Exception):
-        manager.add_stock_in_monitoring("StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5)
+        manager.add_stock_in_monitoring(1, "StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5)
 
 def test_add_stock_with_special_characters(setup_manager):
     manager, _, mock_db = setup_manager
     mock_db.insert_data.return_value = None
-    manager.add_stock_in_monitoring("Special$Char", "005930$", "KR", 1, 70000, 10, 5, 10)
+    manager.add_stock_in_monitoring(1, "Special$Char", "005930$", "KR", 1, 70000, 10, 5, 10)
     mock_db.insert_data.assert_called_once()
 
 
@@ -126,36 +126,36 @@ def test_delete_stock_sql_injection(setup_manager):
 
 def test_update_stock_normal(setup_manager):
     manager, _, mock_db = setup_manager
-    manager.update_stock_in_monitoring("StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5)
+    manager.update_stock_in_monitoring(1, "StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5)
     mock_db.update_data.assert_called_once_with(
-        '''UPDATE INTO monitoring SET trade_round =?, price=?, quantity=?, buy_rate=?, sell_rate=? WHERE code = ?''',
-        (1, 1000, 10, 0.5, 1.5, "123")
+        '''UPDATE INTO monitoring SET id=?, trade_round =?, price=?, quantity=?, buy_rate=?, sell_rate=? WHERE code = ?''',
+        (1, 1, 1000, 10, 0.5, 1.5, "123")
     )
 
 def test_update_stock_invalid_entry(setup_manager):
     manager, _, mock_db = setup_manager
     with pytest.raises(TypeError):
-        manager.update_stock_in_monitoring("StockA", "123", "KR", 1)
+        manager.update_stock_in_monitoring(1, "StockA", "123", "KR", 1)
 
 def test_update_stock_invalid_data(setup_manager):
     manager, _, mock_db = setup_manager
     mock_db.update_data.return_value = None
-    manager.update_stock_in_monitoring("StockA", 123, "KR", "invalid", 1000, 10, 0.5, "invalid")
+    manager.update_stock_in_monitoring(1, "StockA", 123, "KR", "invalid", 1000, 10, 0.5, "invalid")
 
 def test_update_stock_in_monitoring(setup_manager):
     manager, _, mock_db = setup_manager
     mock_db.update_data.return_value = None
-    manager.update_stock_in_monitoring("StockB", "999", "KR", 2, 2000, 10, 0.6, 1.6)
+    manager.update_stock_in_monitoring(1, "StockB", "999", "KR", 2, 2000, 10, 0.6, 1.6)
 
 
 def test_start_monitoring_partial_success(setup_manager, mocker):
     manager, mock_algorithm, mock_db = setup_manager
     mock_db.read_data.return_value = [
-        ("StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5),
-        ("StockB", "456", "KR", 2, 2000, 10, 0.6, 1.6)
+        (1, "StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5),
+        (1, "StockB", "456", "KR", 2, 2000, 10, 0.6, 1.6)
     ]
     mock_algorithm.run_algorithm.side_effect = [Exception("error"),
-mocker.Mock(QueryOp=QueryOp.DELETE, MonitoringData=MonitoringData("StockB", "456", CountryCode.KR, 2, 2000, 10, 0.6, 1.6)),
+mocker.Mock(QueryOp=QueryOp.DELETE, MonitoringData=MonitoringData(1, "StockB", "456", CountryCode.KR, 2, 2000, 10, 0.6, 1.6)),
     ]
     with pytest.raises(Exception):
         manager.start_monitoring()
@@ -170,13 +170,13 @@ def test_start_monitoring_normal(setup_manager, mocker):
 
     # 가짜 데이터 생성
     mock_db.read_data.return_value = [
-        ("StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5),
-        ("StockB", "456", "KR", 2, 2000, 10, 0.6, 1.6)
+        (1, "StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5),
+        (1, "StockB", "456", "KR", 2, 2000, 10, 0.6, 1.6)
     ]
 
     mock_algorithm.run_algorithm.side_effect = [
-        mocker.Mock(QueryOp=QueryOp.UPDATE, MonitoringData=MonitoringData("StockA", "123", CountryCode.KR, 1, 1000, 10, 0.5, 1.5)),
-        mocker.Mock(QueryOp=QueryOp.DELETE, MonitoringData=MonitoringData("StockB", "456", CountryCode.KR, 2, 2000, 10, 0.6, 1.6)),
+        mocker.Mock(QueryOp=QueryOp.UPDATE, MonitoringData=MonitoringData(1, "StockA", "123", CountryCode.KR, 1, 1000, 10, 0.5, 1.5)),
+        mocker.Mock(QueryOp=QueryOp.DELETE, MonitoringData=MonitoringData(1, "StockB", "456", CountryCode.KR, 2, 2000, 10, 0.6, 1.6)),
     ]
 
     # 1. 정상 작동 확인
@@ -195,13 +195,13 @@ def test_start_monitoring_empty(setup_manager, mocker):
 
     # 가짜 데이터 생성
     mock_db.read_data.return_value = [
-        ("StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5),
-        ("StockB", "456", "KR", 2, 2000, 10, 0.6, 1.6)
+        (1, "StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5),
+        (1, "StockB", "456", "KR", 2, 2000, 10, 0.6, 1.6)
     ]
 
     mock_algorithm.run_algorithm.side_effect = [
-        mocker.Mock(QueryOp=QueryOp.UPDATE, MonitoringData=MonitoringData("StockA", "123", CountryCode.KR, 1, 1000, 10, 0.5, 1.5)),
-        mocker.Mock(QueryOp=QueryOp.DELETE, MonitoringData=MonitoringData("StockB", "456", CountryCode.KR, 2, 2000, 10, 0.6, 1.6)),
+        mocker.Mock(QueryOp=QueryOp.UPDATE, MonitoringData=MonitoringData(1, "StockA", "123", CountryCode.KR, 1, 1000, 10, 0.5, 1.5)),
+        mocker.Mock(QueryOp=QueryOp.DELETE, MonitoringData=MonitoringData(1, "StockB", "456", CountryCode.KR, 2, 2000, 10, 0.6, 1.6)),
     ]
 
   # 5. DB 연산 실패 시 처리
@@ -215,8 +215,8 @@ def test_start_monitoring_algorithm_excaption(setup_manager, mocker):
 
     # 가짜 데이터 생성
     mock_db.read_data.return_value = [
-        ("StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5),
-        ("StockB", "456", "KR", 2, 2000, 10, 0.6, 1.6)
+        (1, "StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5),
+        (1, "StockB", "456", "KR", 2, 2000, 10, 0.6, 1.6)
     ]
     # 3. 알고리즘 예외 발생
     mock_algorithm.run_algorithm.side_effect = Exception("Algorithm error")
@@ -228,9 +228,9 @@ def test_start_monitoring_cpu(setup_manager, mocker):
 
     # 가짜 데이터 생성
     mock_db.read_data.return_value = [
-        ("StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5),
-        ("StockB", "456", "KR", 2, 2000, 10, 0.6, 1.6),
-        ("StockC", "789", "KR", 1, 3000, 10, 0.6, 1.6) 
+        (1, "StockA", "123", "KR", 1, 1000, 10, 0.5, 1.5),
+        (1, "StockB", "456", "KR", 2, 2000, 10, 0.6, 1.6),
+        (1, "StockC", "789", "KR", 1, 3000, 10, 0.6, 1.6) 
     ]
     # 4. 쓰레드 풀 동작 확인
     mocker.patch("os.cpu_count", return_value=2)
