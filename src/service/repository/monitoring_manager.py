fcc17f41eba07011b56bbf4acca071210589c544
Junghyun99 <swqasw12345@gmail.com>
2024-12-20 23:01:29 +0900

    monitoring_manager.py 업데이트

--- src/service/repository/monitoring_manager.py
+++ src/service/repository/monitoring_manager.py
@@ -53,7 +53,7 @@ class MonitoringManager(YamlManager):
             errors = []
             stocks = self.read_all_stocks(self.COUNTRY_CODE.value)
             self.logger.log_info("Moni 1. start_monitoring")
-            self.logger.log_debug("  - max_core %s, stock count %s", max_core, len(stock))
+            self.logger.log_debug("  - max_core %s, stock count %s", max_core, len(stocks))
             with ThreadPoolExecutor(max_workers=max_core) as executor:
                 futures = [executor.submit(self.algorithm.run_algorithm, MonitoringData(*stock)) for stock in stocks]
 
